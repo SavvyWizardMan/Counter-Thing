c1757203c9c8438e375fbd8ee851c7a5ae8dbd2a
SavvyWizard <174541572+SavvyWizardMan@users.noreply.github.com>
2024-07-29 20:38:23 -0700

    content for counter

---
+++ README.md
@@ -0,0 +1,3 @@
+# Counter-Thing
+
+A counter project I decided to do
---
+++ script.js
@@ -0,0 +1,15 @@
+const buttonAdd = document.querySelector('.add');
+const buttonMinus = document.querySelector('.minus');
+const spanText = document.querySelector('.text');
+let num = Number(spanText.textContent);
+
+buttonAdd.addEventListener('click', () => {
+    num += 1;
+    spanText.textContent = num;
+    console.log('hi');
+});
+
+buttonMinus.addEventListener('click', () => {
+    num -= 1;
+    spanText.textContent = num;
+});
